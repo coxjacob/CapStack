c167a53986d62b961d6a8f413dcf9b77c64e1b16
“coxjacob” <jacobcox1974@gmail.com>
2025-09-09 23:51:43 -0400

    Added parsing for days and saves on zillow

--- app/search.py
+++ app/search.py
@@ -1,23 +1,59 @@
 from typing import Any, List
+from pathlib import Path
 
 import curl_cffi   # used for requesting data from web pages
 from bs4 import BeautifulSoup as bs  #used to pard web page data
-
-#from curl_cffi import requests
 import json
+import re
 
-address = "3840-Boulder-Creek-Rd-Martinez-GA-30907/"
-url = "https://www.zillow.com/homedetails/"
+
 
 #r = curl_cffi.get("https://tls.browserleaks.com/json", impersonate="chrome")
 #print(r.json())
 
 # Extract address information from Zillow
-r = curl_cffi.get(url+address, impersonate="chrome")
+def retrieve_data(target_url: str, address: str) -> str:
+    r = curl_cffi.get(target_url + address, impersonate="chrome", headers={"Accept-Language": "en-US,en;q=0.9"})
+    #r = curl_cffi.get(r"https://www.zillow.com/homedetails/3840-Boulder-Creek-Rd-Martinez-GA-30907/83373999_zpid/",
+    #                   impersonate="chrome", headers={"Accept-Language": "en-US,en;q=0.9"})
+    return r.text
 
-soup = bs(r.text, 'html.parser')
+def write_to_file(data: str, filename: str) -> None:
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+    print(f"Data written to {filename}")
 
+def parse_days_saves(data: str) -> List[Any]:
+    days = re.search(r'(\d+)\s+dayson', data)
+    saves = re.search(r'(\d+)\s*saves', data)
+    return [days.group(1) if days else None, saves.group(1) if saves else None]
 
-#print(soup.extract())
-print(soup.find_all('text'))
+if __name__ == "__main__":
+    if not Path("data.txt").is_file():
+        print("File not found, retrieving data...")
+        address = "3840-Boulder-Creek-Rd-Martinez-GA-30907/83373999_zpid/"
+        target_url = "https://www.zillow.com/homedetails/"
+        raw_text = retrieve_data(target_url, address)
+        print(type(raw_text))
+        with open("data.txt", 'w') as f:
+            f.write(raw_text)
+    else:
+        print("File found, reading data...")
+        with open("data.txt", 'r') as f:
+            raw_text = f.read()
+    
+    soup = bs(raw_text, 'html.parser')
+    # Collect days on market and saves
+    days = soup.find_all('dl', {'class':'styles__StyledOverviewStats-fshdp-8-111-1__sc-1x11gd9-0 kpgmGL'})
+    for day in days:
+        print(day.text)
+        days_saves = parse_days_saves(day.text)
+        print(days_saves) # [days, saves]
+
+    # This code still does not work, need to find a way to extract Zestimate value
+    zestimate = soup.find_all('span', {'class':"Text-c11n-8-111-1__sc-aiai24-0 sc-WKhSL hZAvJt ivuDGZ"})
+    print(zestimate)
+    for z in zestimate:
+        print(z.text)
+
 
